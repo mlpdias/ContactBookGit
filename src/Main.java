0708e2c0272a312968d0b719eedbadc8c179a70a
mpoge <mlp.dias@campus.fct.unl.pt>
2026-09-20 12:45:45 +0100

    Added a "." at the end of the NUMBER_NOT_EXIST message to be coherent with the tests given outputs.

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
-    public static final String NUMBER_NOT_EXIST = "Phone number does not exist";
+    public static final String NUMBER_NOT_EXIST = "Phone number does not exist.";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
