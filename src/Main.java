a0e1e660520fec33df194ae3602ad436e00b19f9
mpoge <mlp.dias@campus.fct.unl.pt>
2026-09-20 13:01:39 +0100

    Added the messages for the "EP" command and it's main function.

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
     public static final String GET_NUMBER     = "GN";
+    public static final String EXIST_DUPLICATED_PHONES = "EP";
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -26,6 +27,8 @@ public class Main {
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
     public static final String NUMBER_NOT_EXIST = "Phone number does not exist.";
+    public static final String EXIST_SHARED_PHONES = "There are contacts that share phone numbers.";
+    public static final String NO_SHARED_PHONES = "All contacts have different phone numbers.";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -57,6 +60,9 @@ public class Main {
                     break;
                 case GET_NUMBER:
                     getNumber(in, cBook);
+                    break;
+                case EXIST_DUPLICATED_PHONES:
+                    verifyNumbers(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -162,6 +168,14 @@ public class Main {
             System.out.println(name);
         } else {
             System.out.println(NUMBER_NOT_EXIST);
+        }
+    }
+
+    private static void verifyNumbers(ContactBook cBook) {
+        if(cBook.allDifferent()) {
+            System.out.println(NO_SHARED_PHONES);
+        } else {
+            System.out.println(EXIST_SHARED_PHONES);
         }
     }
 }
