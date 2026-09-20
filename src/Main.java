f9830d815a439d768635c0e2b60208f44442fbda
mpoge <mlp.dias@campus.fct.unl.pt>
2026-09-20 11:54:17 +0100

    Added the strings for the command GET_NUMBER "GN" and it's respective error message.

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    public static final String GET_NUMBER     = "GN";
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -24,6 +25,7 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String NUMBER_NOT_EXIST = "Phone number does not exist";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
